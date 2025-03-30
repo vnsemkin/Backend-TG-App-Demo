0a70542f01f5319a1f501363e68e325b23e4f3ff
vnsemkin <vnsemkin@gmail.com>
2025-03-30 12:19:17 +0300

    Initial commit

---
+++ src/test/java/com/example/backendfordemoalenaproject/BackendForDemoAlenaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendfordemoalenaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendForDemoAlenaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
