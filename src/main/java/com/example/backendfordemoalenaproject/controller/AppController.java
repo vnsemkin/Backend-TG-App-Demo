7a6fa34799c766da15681c43e64016bd403312eb
vnsemkin <vnsemkin@gmail.com>
2025-03-31 11:34:28 +0300

    Create bot template with deploy docker container

--- src/main/java/com/example/backendfordemoalenaproject/controller/AppController.java
+++ src/main/java/com/example/backendfordemoalenaproject/controller/AppController.java
@@ -2,18 +2,21 @@ package com.example.backendfordemoalenaproject.controller;
 
 import com.example.backendfordemoalenaproject.model.RequestDataDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController()
 @RequestMapping("/api/v1")
 public class AppController {
     @GetMapping("/send-message")
-    public String hello(@RequestBody RequestDataDto requestDataDto) {
+    public String getHello(@RequestBody RequestDataDto requestDataDto) {
         log.info("Get request data: {}", requestDataDto);
-        return "Hello from backend";
+        return "Get req received: Hello from backend";
+    }
+
+    @PostMapping("/send-message")
+    public String postHello(@RequestBody RequestDataDto requestDataDto) {
+        log.info("Get request data: {}", requestDataDto);
+        return "Post req received: Hello from backend";
     }
 }
