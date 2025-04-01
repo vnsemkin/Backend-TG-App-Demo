ba9ccc6a363515ea1bbda7f2f7aa7e266fdb2163
vnsemkin <vnsemkin@gmail.com>
2025-04-01 21:28:11 +0300

    Add mini app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import * as test from "node:test";
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Telegram Bot Mini-App/i);
   expect(linkElement).toBeInTheDocument();
 });
